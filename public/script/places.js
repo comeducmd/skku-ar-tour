2402339582402cb04db7007a8745431388107196
Heedong Kim <ruthetum@gmail.com>
2021-03-05 23:41:39 +0900

    [FEAT] 클릭 이벤트 테스트

--- public/script/places.js
+++ public/script/places.js
@@ -25,7 +25,7 @@ function staticLoadPlaces() {
 }
 
 function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
+    const scene = document.querySelector('a-scene');
 
     places.forEach((place) => {
         let latitude = place.location.lat;
@@ -37,6 +37,32 @@ function renderPlaces(places) {
         icon.setAttribute('src', '#marker');
         icon.setAttribute('scale', '30 30 30');
         icon.setAttribute('position', `${position}`);
+        icon.setAttribute('name', place.name);
+
+        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+        const clickListener = function(ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('name');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
+        icon.addEventListener('click', clickListener);
+
         scene.appendChild(icon);
     });
 }
