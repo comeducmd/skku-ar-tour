0cedbcabc0a882f895c8d2000ccfcc31b33104b8
Heedong Kim <ruthetum@gmail.com>
2021-03-11 16:52:44 +0900

    [FEAT] place id 추가

--- public/script/places.js
+++ public/script/places.js
@@ -6,6 +6,7 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
+            id: 'place1',
             name: '중앙학술정보관',
             location: {
                 lat: 37.58600973854398,
@@ -14,6 +15,7 @@ function staticLoadPlaces() {
             position: '0 0 -30',
         },
         {
+            id: 'place2',
             name: '금잔디광장',
             location: {
                 lat: 37.588227,
@@ -28,40 +30,19 @@ function renderPlaces(places) {
     const scene = document.querySelector('a-scene');
 
     places.forEach((place) => {
+        let id = place.id;
         let latitude = place.location.lat;
         let longitude = place.location.lng;
         let position = place.position;
 
         const icon = document.createElement('a-image');
+        icon.id = id;
+        icon.setAttribute('place', '');
         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
         icon.setAttribute('src', '#marker');
         icon.setAttribute('scale', '30 30 30');
         icon.setAttribute('position', `${position}`);
         icon.setAttribute('name', place.name);
-
-        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-        const clickListener = function(ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
-
-            const name = ev.target.getAttribute('name');
-
-            const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-            if (el && el === ev.target) {
-                const label = document.createElement('span');
-                const container = document.createElement('div');
-                container.setAttribute('id', 'place-label');
-                label.innerText = name;
-                container.appendChild(label);
-                document.body.appendChild(container);
-
-                setTimeout(() => {
-                    container.parentElement.removeChild(container);
-                }, 1500);
-            }
-        };
-        icon.addEventListener('click', clickListener);
 
         scene.appendChild(icon);
     });
