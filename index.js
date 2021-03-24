8dfd47aa67610798b19ab9ce6d57b597c60a78d5
Heedong Kim <ruthetum@gmail.com>
2021-03-24 15:02:02 +0900

    [FEAT] 환경변수 설정

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('./config/express');
 const { logger } = require('./config/winston');
+require('dotenv').config();
 
-const port = 4243;
+const port = process.env.PORT;
 express().listen(port);
 logger.info(`Server Start At Port ${port}`);
