c8cda5dff2e4c24a8a1b0caa703fd03415f5bd0e
Serin <bcode429@gmail.com>
2021-03-24 15:58:39 +0900

    [FIX] stylesheets 폴더명 변경

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const { logger } = require('./config/winston');
 require('dotenv').config();
 
 const port = process.env.PORT;
+
+console.log('포트', port);
 express().listen(port);
 logger.info(`Server Start At Port ${port}`);
