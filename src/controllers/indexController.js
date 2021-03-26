0a6bb9ed1b06f1ef591fa54be55d55a21c59b876
Heedong Kim <ruthetum@gmail.com>
2021-03-26 11:00:47 +0900

    [FEAT] LocalStorage 초기화용(임시)
    /clear

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -4,3 +4,7 @@ exports.default = async function (req, res) {
     const variable = 5;
     return res.render('index.ejs', { v: variable});
 };
+
+exports.clear = async function (req, res) {
+    return res.render('clear.ejs');
+};
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,4 +1,5 @@
 module.exports = function(app){
     const index = require('../controllers/indexController');
     app.get('/', index.default);
+    app.get('/clear', index.clear);
 };
