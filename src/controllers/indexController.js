81e61ea2916e91e00daa9d282e5211fa1a27a099
Heedong Kim <ruthetum@gmail.com>
2021-03-26 13:55:25 +0900

    [Feat] 페이지 로깅

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,9 +2,11 @@ const {logger} = require('../../config/winston');
 
 exports.default = async function (req, res) {
     const variable = 5;
+    logger.info('Default');
     return res.render('index.ejs', { v: variable});
 };
 
 exports.clear = async function (req, res) {
+    logger.info('Clear');
     return res.render('clear.ejs');
 };
